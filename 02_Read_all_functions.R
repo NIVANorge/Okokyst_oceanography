6e6f6f058edd0e8833cb815def7c04faf99672db
Dag Hjermann <dhj@niva.no>
2019-03-04 15:14:35 +0100

    Add functions

--- 02_Read_all_functions.R
+++ 02_Read_all_functions.R
@@ -46,5 +46,55 @@ okokyst_read_nc <- function(fn, variable, ctd_variable, report = FALSE,
   df2
   }
 
+#
+# Make ggplot-ready data (including interpolation)
+#
+okokyst_make_plotdata <- function(data, varname){
+  data <- as.data.frame(data)
+  sel <- complete.cases(data[,c("Time", "Depth", varname)])
+  data <- dplyr::rename(data, "z" = varname)
+  # Interpolation
+  data$Time2 <- as.numeric(data$Time)/86400    # convert to day-scale, for better interpolation
+  interpol <- with(data[sel,], akima::interp(Time2, Depth, z, nx = 100, ny = 100, linear = FALSE))   # spline
+  # Reshaping data
+  dimnames(interpol$z)[[2]] <- interpol$y
+  as.data.frame(interpol$z) %>%
+    data.frame(x = interpol$x, .) %>%
+    tidyr::gather(key = "y", value = "z", -x) %>%
+    mutate(Depth = as.numeric(sub("X", "", y)),
+           Time = as.POSIXct(x*86400, origin = "1970-01-01")) %>%
+    select(-y)
+  }
 
+# df_plot <- okokyst_make_plotdata(df, "salt")
+
+okokyst_plot <- function(data, varname, ctd_variable, title = "", binwidth = 1, limits = c(NA,NA),
+                         color_ctdtime = "black"){
+  df_plot <- okokyst_make_plotdata(data, varname)
+  gg <- ggplot(df_plot, aes(Time, Depth)) +
+    geom_raster(aes(fill = z), interpolate = F, hjust = 0.5, vjust = 0.5) +
+    geom_contour(aes(z = z), binwidth = binwidth) + 
+    scale_y_reverse() +
+    scale_fill_gradientn(varname, colours = fields::tim.colors(16), limits = limits)
+  if (ctd_variable){
+    gg <- gg + geom_vline(xintercept = unique(data$Time), color = color_ctdtime)
+  } else {
+    gg <- gg + geom_point(data = data, aes(Time, Depth), color = "white", size = 0.5)
+  }
+  if (title != "")
+    gg <- gg + ggtitle(title)
+  gg
+}
+
+okokyst_plot_points <- function(data, varname, ctd_variable, title = "", binwidth = 1, limits = c(NA,NA),
+                         color_ctdtime = "black"){
+  df_plot <- data %>% rename("z" = varname)
+  gg <- ggplot(df_plot, aes(Time, Depth)) +
+    geom_point(aes(color = z)) +
+    scale_y_reverse() +
+    scale_color_gradientn(varname, colours = fields::tim.colors(16), limits = limits)
+  if (title != "")
+    gg <- gg + ggtitle(title)
+  gg
+}
 
