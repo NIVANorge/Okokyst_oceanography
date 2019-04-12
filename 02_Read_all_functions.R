b77e579299b038607ce350e7c962a2339b406c4c
Dag_Hjermann <dhj@niva.no>
2019-04-12 15:41:25 +0200

    Add read_depth_nu, script 02..functions

--- 02_Read_all_functions.R
+++ 02_Read_all_functions.R
@@ -3,6 +3,7 @@ library(ncdf4)
 
 #
 # Read one variable from one okokyst file
+# - Used in script 02 and 08 (at least)
 # Set 'ctd_variable' = TRUE if the variable is a CTD variable (values for every 1 m) such as temp, salinity
 # Set 'ctd_variable' = FALSE if the variable is a water sample variable (values for 0,5,10,20 ..m) such as 
 #   chlorophyll, nutrients
@@ -12,7 +13,8 @@ library(ncdf4)
 #   df <- okokyst_read_nc("VR51.nc", "KlfA", ctd_variable = FALSE)
 #
 okokyst_read_nc <- function(fn, variable, ctd_variable, report = FALSE, 
-                            folder_data = "K:/Avdeling/214-Oseanografi/DATABASER/OKOKYST_2017/ncbase_OKOKYST"){
+                            folder_data = "K:/Avdeling/214-Oseanografi/DATABASER/OKOKYST_2017/ncbase_OKOKYST",
+                            read_depth_nut = TRUE){
   if (report)
     cat(fn, variable, "\n")
   ncin <- nc_open(paste0(folder_data, "/", fn))
@@ -21,7 +23,8 @@ okokyst_read_nc <- function(fn, variable, ctd_variable, report = FALSE,
   time <- ncvar_get(ncin, "time")
   time <- as.POSIXct(time + 1, tz = "UTC", origin = "1970-01-01")   # add 1 second in order to get a time, not  date
   depth <- ncvar_get(ncin, "depth")
-  depth_nut <- ncvar_get(ncin, "depth_nut")
+  if (read_depth_nut)
+    depth_nut <- ncvar_get(ncin, "depth_nut")
   Y <- ncvar_get(ncin, variable)
   nc_close(ncin)
   # Pick depth variable depending on whether we hav a CTD variable or a water sample variable
