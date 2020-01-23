e5c97914b284d489011c256f13b7b8a25b7e39d4
Dag_Hjermann <dhj@niva.no>
2020-01-23 09:45:56 +0100

    Yet another change

--- 01_Reading_data.R
+++ 01_Reading_data.R
@@ -1,7 +1,20 @@
 
 #
-# This is my new file
+# Read and make som test plots
 #
 
+#
+# Read data
+#
 library(readxl)
 data <- read_excel("Some_data.xlsx")
+
+#
+# Test plots ----
+#
+plot(data$x)
+plot(data$y)
+plot(y ~ x, data)
+
+
+
