e8f74da556cce97b418b0c1cb742df3c0a8af531
Dag_Hjermann <dhj@niva.no>
2020-02-15 17:37:54 +0100

    Add several aggregate + plot functions, scr 12

--- 12_QA_2019_from_excel_functions.R
+++ 12_QA_2019_from_excel_functions.R
@@ -1,6 +1,8 @@
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
 #
 # READING "WIDE" AQUAMONITOR DATA ----
 #
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
 
 AqMexport_read_waterchemistry <- function(filename, reformat_long = TRUE, remove_duplicates = TRUE, sheetname = "WaterChemistry"){
   # Read top line, only for the column names
@@ -26,7 +28,7 @@ AqMexport_read_waterchemistry <- function(filename, reformat_long = TRUE, remove
     df_chem <- AqMexport_reformat_long(df_chem, remove_duplicates = remove_duplicates)
   
   df_chem 
-  }
+}
 
 # Note hard-coded columns "ProjectId:Depth2"
 AqMexport_reformat_long <- function(dat, remove_duplicates = TRUE){
@@ -43,7 +45,7 @@ AqMexport_reformat_long <- function(dat, remove_duplicates = TRUE){
     dat_long <- AqMexport_remove_duplicates(dat_long)
   dat_long
 }
-  
+
 AqMexport_remove_duplicates <- function(dat){
   dat %>%
     filter(!is.na(Value)) %>%
@@ -57,9 +59,14 @@ AqMexport_remove_duplicates <- function(dat){
 
 
 
+
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
 #
 # NUTRIENT PLOTS ----
 #
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+
+
 #
 # Plotting nutrient time series, based on "long" Aquamonitor data   
 #
@@ -149,8 +156,242 @@ nutrient_plot_p <- function(stationcode, depth, years = 2017:2019, print_plot = 
   
   invisible(gg)
   
+}
+
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+#
+# NUTRIENT MEANS ----
+#
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+
+
+# Functions for getting weighted means for each nutrient/season
+# By "weighted" we mean weighted over depth
+#
+
+#
+# Weighted mean over depth, but for each time (and each station/variable)
+#
+# Weights are given by "depths_weights"
+# "depths_weights" may be in 0-1 range, 0-100 or just as relative weights
+#   (they are divided by the sum anyway)
+#
+NIVAklass_get_mean_overdepth <- function(years, data, vars, 
+                                         months,
+                                         depths = c(0,5,10),
+                                         depths_weights = c(1,2,1)){
+  lookuptable_weight <- 
+    data.frame(Depth1 = depths,
+               Weight = depths_weights/sum(depths_weights)
+    )
+  df <- data %>%
+    mutate(Depth1 = ifelse(Depth1 == 0.5, 0, Depth1)) %>%
+    filter(Variable %in% vars & 
+             Depth1 %in% depths &
+             month(Time) %in% months & year(Time) %in% years) %>% 
+    left_join(lookuptable_weight, by = "Depth1")
+  df %>%
+    group_by(StationCode, Variable, Time) %>%
+    summarize(Value = sum(Value*Weight)) %>%
+    ungroup()
+}
+
+# Example
+if (FALSE){
+  df <- NIVAklass_get_mean_overdepth(
+    years = 2017:2019,
+    data = dat2,
+    vars = c("TOTP", "PO4-P", "TOTN", "NO3+NO2-N", "NH4-N"),
+    months = c(6,7,8))
+  df
+}
+#
+# Seasonal weighted means
+# I.e., mean over depth and time (for each station/variable within a given season)
+#
+NIVAklass_get_meanvalues <- function(years, data, vars, 
+                                     months, 
+                                     depths = c(0,5,10),
+                                     depths_weights = c(1,2,1)){
+  df <- NIVAklass_get_mean_overdepth(years = years, 
+                                     data = data, 
+                                     vars = vars, 
+                                     months = months, 
+                                     depths = depths,
+                                     depths_weights = depths_weights)
+  
+  df %>%
+    group_by(StationCode, Variable) %>%
+    summarize(Value = mean(Value)) %>%
+    mutate(Variable = factor(Variable, levels = vars)) %>%
+    arrange(Variable)
+}
+
+# Example
+if (FALSE){
+  # debugonce(NIVAklass_get_meanvalues)
+  NIVAklass_get_meanvalues(
+    years = 2017:2019,
+    data = dat2,
+    vars = c("TOTP", "PO4-P", "TOTN", "NO3+NO2-N", "NH4-N"),
+    months = c(6,7,8)
+  )
+}
+
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+# Functions for getting summer 
+# and winter values of nutrients
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+
+# Total fosfor (μg P/l), vinter
+# Fosfat-fosfor (μg P/l), vinter
+# Total nitrogen (μg N/l), vinter
+# Nitrat-nitrogen (μg N/l), vinter
+# Ammonium-nitrogen (μg N/l), vinter
+
+
+# Get summer values
+NIVAklass_summervalues <- function(years, data, ...){
+  NIVAklass_get_meanvalues(
+    years = years, 
+    data = data, 
+    # Same order as in NIVAklass Excel sheet:
+    vars = c("TOTP", "PO4-P", "TOTN", "NO3+NO2-N", "NH4-N"),
+    months = c(6,7,8),
+    ...
+  )
+}
+
+# Get winter values
+NIVAklass_wintervalues <- function(years, data, ...){
+  NIVAklass_get_meanvalues(
+    years = years, 
+    data = data, 
+    # Same order as in NIVAklass Excel sheet:
+    vars = c("TOTP", "PO4-P", "TOTN", "NO3+NO2-N", "NH4-N"),
+    months = c(1,2,12),
+    ...
+  )
+}
+
+# Get summer + winter values and combine them (row-wise)
+NIVAklass_nutrientvalues <- function(years, data, ...){
+  df_nut_summer <- NIVAklass_summervalues(years, data, ...) %>%
+    mutate(Season = "Summer") %>%
+    select(StationCode, Season, Variable, Value)
+  df_nut_winter <- NIVAklass_wintervalues(years, data, ...) %>%
+    mutate(Season = "Winter") %>%
+    select(StationCode, Season, Variable, Value)
+  bind_rows(df_nut_summer, df_nut_winter) %>%
+    arrange(StationCode, Season, Variable)
+}
+
+
+
+
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+#
+# OXYGEN PLOTS ----
+#
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+
+#
+# For plotting time series, typically of oxygen
+#
+# data - must have variables Date, Category and one variable named in "variable"
+#      - may optionally also have a variable named in "category_variable"
+#       (Category can typically be "bottom" and "O2-minimum" by\ut can also be 
+#        station code/name, or both)
+# variable - typically Oksygen or Oksygenmetning
+# category_variable - typically "Position"
+# 
+plot_ts <- function(data, 
+                    years, 
+                    time_limits = NULL,      # not necessary if years is given
+                    y_variable = "Oksygen",
+                    category_variable = NULL,  # gives 
+                    quality_type = "Concentration",  # "Concentration", "Saturaion", or similar
+                    quality_lines_y = NULL,   # can be used to override 'quality_type'
+                    quality_labels_y = NULL,  # must be given if quality_lines_y is given
+                    quality_labels = NULL,     # must be given if quality_lines_y is given
+                    qualitylabel_x = -0.05,
+                    title = NULL){ # 0 to 1; position of "SG", "G" etc.
+  # Limits of x axis
+  if (is.null(time_limits)){
+    time_limits <- c(ymd(min(years)*10000 + 0101, tz = "GMT"),
+                     ymd(max(years)*10000 + 1231, tz = "GMT"))
+  }
+  # For "winter shading":
+  # the first winter starts from 10.01 the year before the first year  
+  time_1oct = ymd(seq(min(years)-1, max(years))*10000 + 1001,  
+                  tz = "GMT")
+  # the last winter ends 30.04 the year after the last year  
+  time_1may = ymd(seq(min(years), max(years)+1)*10000 + 0430, 
+                  tz = "GMT")
+  # Position 
+  qualitylabel_coor <- as.POSIXct(
+    as.numeric(time_limits[1]) + qualitylabel_x*diff(as.numeric(time_limits)), 
+    origin = "1970-01-01", tz = "GMT"
+  )
+  time_limits
+  # If quality_type = "Concentration" or something similar
+  if (is.null(quality_lines_y)){
+    if (grepl("conc", quality_type, ignore.case = TRUE)){
+      quality_labels = c("SD","D","M","G","MG")
+      quality_lines_y <- seq(1.5, 4.5, 1) 
+      quality_labels_y <- seq(1, 5)
+      # If quality_type = "Saturation" or simething similar
+    } else if (grepl("sat", quality_type, ignore.case = TRUE)){
+      quality_labels = c("SD","D","M","G","MG")
+      quality_lines_y <- seq(20, 65, 15) 
+      quality_labels_y <- seq(20 - 7.5, 65 + 7.5, 15)
+    }
+  }
+  if (is.null(category_variable)){
+    gg <- ggplot(dat_oxygen, aes(Date, .data[[y_variable]]))    
+  } else {
+    gg <- ggplot(dat_oxygen, aes(Date, .data[[y_variable]], 
+                                 color = .data[[category_variable]]))    
+  }
+  gg <- gg + 
+    # Winter shading
+    annotate("rect", xmin = time_1oct, xmax = time_1may, 
+             ymin = -Inf, ymax = Inf,
+             alpha = .2) +
+    # Main plot
+    geom_line() +
+    geom_point()
+  # If quality_type is not "Concentration" or "Saturaion" AND 
+  #   quality_lines_y is not given
+  if (!is.null(quality_lines_y)){
+    gg <- gg +
+      geom_hline(yintercept = quality_lines_y, 
+                 linetype = "dashed", color = "red3") +
+      annotate("text", 
+               x = qualitylabel_coor, 
+               y = quality_labels_y, 
+               label = quality_labels, 
+               hjust = 0.5) #+
+  }
+  # coord_cartesian(xlim = time_limits)  
+  gg <- gg + 
+    labs(y = y_variable) + 
+    theme_bw()
+  if (!is.null(category_variable))
+    gg <- gg +
+    scale_color_discrete(category_variable)
+  gg  
   }
 
+# EXamples (for oxygen)
+if (FALSE){
+  plot_ts(dat_oxygen, 2017:2019, 
+          y_variable = "Oksygen", category_variable = "Position", 
+          quality_type = "Concentration")
+  plot_ts(dat_oxygen, 2017:2019,  category_variable = "Position",
+          y_variable = "Oksygenmetning", 
+          quality_type = "Saturation")
+}
 
 #
 # TESTS
@@ -158,9 +399,12 @@ nutrient_plot_p <- function(stationcode, depth, years = 2017:2019, print_plot = 
 # nutrient_plot_n("VT67", 0)
 # nutrient_plot_p("VT3", 10)
 
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
 #
 # FUNCTIONS FOR CHECKING SUMS ----
 #
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+
 # Check that the sum of N components is not larger than TOTN
 # Check that the sum of P components is not larger than TOTP
 #
@@ -288,10 +532,10 @@ check_sums_p1 <- function(data,
   invisible(df)
   
 }
-  
-  # ggplot(df, aes(Check)) + geom_histogram()
-  
-  
+
+# ggplot(df, aes(Check)) + geom_histogram()
+
+
 # Check whether TotP is smaller or equal to phosphate     
 check_sums_p2 <- function(data, 
                           max_n = 30){   # Max number of cases to show
@@ -319,6 +563,6 @@ check_sums_p2 <- function(data,
   
   invisible(df)
   
-  }
+}
 
 
